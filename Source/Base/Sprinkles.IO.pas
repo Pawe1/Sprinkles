e20bae4d8fab281fc55c2e51b4b2d50d9ece1b82
PC <>
2017-06-06 14:33:22 +0200

    Just a tiny experiment

--- Source/Base/Sprinkles.IO.pas
+++ Source/Base/Sprinkles.IO.pas
@@ -4,6 +4,11 @@ unit Sprinkles.IO;
 
 interface
 
+uses
+  System.Classes,
+  System.SysUtils,   // EDirectoryNotFoundException, TEncoding, ... 
+  System.RegularExpressions;   // TRegEx
+
 type
   TFileSystemFamily = (Unix, Windows);
 
@@ -22,12 +27,40 @@ type
     function Combine(const APath1, APath2: string): string; inline;
   end;
 
+  TRegExTextSearcher = class abstract
+  private
+    FLine: string;
+    FPattern: string;
+    FReader: TTextReader;
+    FRegEx: TRegEx;
+    function IsMatch(const ALine: string): Boolean;
+  public
+    function FindFirstLine: Boolean; virtual;
+    function FindNextLine: Boolean; virtual; abstract;
+    function ReadLine: string;
+    property Pattern: string write FPattern;
+  end;
+
+  TRegExStreamSearcher = class(TRegExTextSearcher)
+  private
+  public
+    constructor Create(Stream: TStream); overload;
+    constructor Create(Stream: TStream; DetectBOM: Boolean); overload;
+    constructor Create(Stream: TStream; Encoding: TEncoding; DetectBOM: Boolean = False; BufferSize: Integer = 4096); overload;
+    constructor Create(const Filename: string); overload;
+    constructor Create(const Filename: string; DetectBOM: Boolean); overload;
+    constructor Create(const Filename: string; Encoding: TEncoding; DetectBOM: Boolean = False; BufferSize: Integer = 4096); overload;
+    destructor Destroy; override;
+    function FindFirstLine: Boolean; override;
+    function FindNextLine: Boolean; override;
+  end;
+
 procedure CheckDirectoryExists(const APath: string; AFollowLink: Boolean = True);
 
 implementation
 
 uses
-  System.SysUtils,
+  System.RTLConsts,   // SPathNotFound
   System.IOUtils;
 
 resourcestring
@@ -58,6 +91,81 @@ begin
 end;
 
 {$ENDREGION}
+
+constructor TRegExStreamSearcher.Create(Stream: TStream; Encoding: TEncoding; DetectBOM: Boolean; BufferSize: Integer);
+begin
+  FReader := TStreamReader.Create(Stream, Encoding, DetectBOM, BufferSize);
+end;
+
+constructor TRegExStreamSearcher.Create(Stream: TStream; DetectBOM: Boolean);
+begin
+  FReader := TStreamReader.Create(Stream, DetectBOM);
+end;
+
+constructor TRegExStreamSearcher.Create(Stream: TStream);
+begin
+  FReader := TStreamReader.Create(Stream);
+end;
+
+constructor TRegExStreamSearcher.Create(const Filename: string; Encoding: TEncoding; DetectBOM: Boolean; BufferSize: Integer);
+begin
+  FReader := TStreamReader.Create(Filename, Encoding, DetectBOM, BufferSize);
+end;
+
+constructor TRegExStreamSearcher.Create(const Filename: string; DetectBOM: Boolean);
+begin
+  FReader := TStreamReader.Create(Filename, DetectBOM);
+end;
+
+constructor TRegExStreamSearcher.Create(const Filename: string);
+begin
+  FReader := TStreamReader.Create(Filename);
+end;
+
+destructor TRegExStreamSearcher.Destroy;
+begin
+  FReader.Free;
+  inherited;
+end;
+
+function TRegExStreamSearcher.FindFirstLine: Boolean;
+begin
+  inherited;
+  (FReader as TStreamReader).BaseStream.Position := 0;
+  Result := FindNextLine;
+end;
+
+function TRegExStreamSearcher.FindNextLine: Boolean;
+var
+  Line: string;
+begin
+  Result := False;
+  while not (FReader as TStreamReader).EndOfStream do
+  begin
+    Line := FReader.ReadLine;
+    if IsMatch(Line) then
+    begin
+      FLine := Line;
+      Result := True;
+      Break;
+    end;
+  end;
+end;
+
+function TRegExTextSearcher.FindFirstLine: Boolean;
+begin
+  FRegEx := TRegEx.Create(FPattern);
+end;
+
+function TRegExTextSearcher.IsMatch(const ALine: string): Boolean;
+begin
+  Result := FRegEx.IsMatch(ALine);
+end;
+
+function TRegExTextSearcher.ReadLine: string;
+begin
+  Result := FLine;
+end;
 
 procedure CheckDirectoryExists(const APath: string; AFollowLink: Boolean = True);
 begin
